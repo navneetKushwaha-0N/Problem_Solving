72ca5065699bfe545b2059ade5c934f720e4e550
Navneet Kushwaha <117534965+mollenmi@users.noreply.github.com>
2025-03-04 21:30:14 +0530

    It all three numbers are equal, print All numbers are equal

---
+++ Java/EqualNumbersChecker.java
@@ -0,0 +1,11 @@
+public class EqualNumbersChecker {
+    public static void main(String[] args) {
+        int num1 = 5, num2 = 5, num3 = 5;
+
+        if (num1 == num2 && num2 == num3) {
+            System.out.println("All numbers are equal");
+        } else {
+            System.out.println("Numbers are not equal");
+        }
+    }
+}
