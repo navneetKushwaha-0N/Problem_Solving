8356bc3d4c83bbe1fa05d56f4494b5566bb5a421
Navneet Kushwaha <117534965+mollenmi@users.noreply.github.com>
2025-03-05 11:09:12 +0530

    EqualNumbersChecker

--- Java/EqualNumbersChecker.java
+++ Java/EqualNumbersChecker.java
@@ -1,5 +1,5 @@
 
-//It all three numbers are equal, print "All numbers are equal"
+//It all  three numbers are equal, print "All numbers are equal"
 
 public class EqualNumbersChecker {
     public static void main(String[] args) {
@@ -8,7 +8,7 @@ public class EqualNumbersChecker {
         if (num1 == num2 && num2 == num3) {
             System.out.println("All numbers are equal");
         } else {
-            System.out.println("Numbers are not equal");
+            System.out.println("Numbers are not equ");
         }
     }
 }
