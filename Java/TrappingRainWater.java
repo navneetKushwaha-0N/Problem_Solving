d5e779e2716c4a99e72439b6354c86d3dfa025a7
Navneet Kushwaha <117534965+mollenmi@users.noreply.github.com>
2025-03-27 16:32:47 +0530

    problemSolving

---
+++ Java/TrappingRainWater.java
@@ -0,0 +1,37 @@
+public class TrappingRainWater {
+    public static int trap(int[] height) {
+        if (height == null || height.length < 3) {
+            return 0; // No water can be trapped if there are less than 3 bars
+        }
+
+        int n = height.length;
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int waterTrapped = 0;
+
+        while (left <= right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    waterTrapped += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    waterTrapped += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return waterTrapped;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println("Water trapped: " + trap(height));
+    }
+}
