de71e22f858db1c8aa6dab2df46ddec770dca8e1
Navneet Kushwaha <117534965+mollenmi@users.noreply.github.com>
2025-03-28 12:57:46 +0530

    problem solving

--- Java/practice.java
+++ Java/practice.java
@@ -1,5 +1,5 @@
 public class practice{
     public static void main(String[] args){
-        System.out.println("hllo World from practice.java");
+        System.out.println("hello World from practice");
     }
 }
