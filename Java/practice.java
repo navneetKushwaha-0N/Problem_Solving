63f33af122ab64638b28053b7a446187bc1469be
Navneet Kushwaha <117534965+mollenmi@users.noreply.github.com>
2025-03-29 15:15:17 +0530

    problem solving

--- Java/practice.java
+++ Java/practice.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class practice{
     public static void main(String[] args){
-        System.out.println("hello World from practice");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + "!");
+
     }
 }
